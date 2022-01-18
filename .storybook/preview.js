cc9844f81150f59feece6dfff6fce48b2a904132
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-01-18 12:03:20 +0100

    Install and configure storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import "../styles/base.css";
+import "../styles/colors.ccss";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
