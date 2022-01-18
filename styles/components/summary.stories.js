de0c045f8700ce98bb7a5d99a2c8f7e6e426cf9d
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-01-18 20:30:10 +0100

    Adjust BEM and clean components

---
+++ styles/components/summary.stories.js
@@ -0,0 +1,11 @@
+import "./summary.css";
+import HTML from "./summary.html";
+
+export default {
+  title: "Components/ Summary",
+};
+
+export const Summary = () => HTML;
+export const Summary_image = () => HTML;
+export const Summary_text = () => HTML;
+export const Summary_image__design = () => HTML;
